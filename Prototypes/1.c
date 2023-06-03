9d6b8c1b706d6426504947293e26b9b9c308c80c
anshium <anshchablani@gmail.com>
2023-06-03 16:26:09 +0530

    Added some headers and init Graph

--- Prototypes/1.c
+++ Prototypes/1.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Graph_Structure{
+	struct Node** startList;
+	int num_vertices;
+} Graph_Structure;
+
+typedef struct Node{
+	int val;
+	struct Node* next;
+} Node;
+
+typedef Graph_Structure* Graph;
+typedef Node* nodeptr;
+
+Graph initGraph(int num_vertices){
+	Graph g = (Graph)malloc(sizeof(Graph_Structure));
+	g->startList = (Node**)malloc(sizeof(Node*) * num_vertices);
+	g->num_vertices = num_vertices;
+	for(int i = 0; i < num_vertices; i++){
+		g->startList[i] = (Node*)malloc(sizeof(Node));
+	}
+
+	return g;
+}
+
+
+
 int main(){
 
 	return 0;
