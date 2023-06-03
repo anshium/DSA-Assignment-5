ae1e7a08b671402857ce95d92a2f601339d3261c
anshium <anshchablani@gmail.com>
2023-06-03 23:14:14 +0530

    Finally done with Q1

---
+++ Prototypes/1_withInput.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+
+typedef struct Graph_Structure{
+	struct Node** startList;
+	int num_vertices;
+} Graph_Structure;
+
+typedef struct Node{
+	int val;
+	int idkname;
+	int visited;
+	struct Node* next;
+} Node;
+
+typedef Graph_Structure* Graph;
+typedef Node* nodeptr;
+
+typedef int* Array;
+
+Graph initGraph(int num_vertices){
+	Graph g = (Graph)malloc(sizeof(Graph_Structure));
+	g->startList = (Node**)malloc(sizeof(Node*) * num_vertices);
+	g->num_vertices = num_vertices;
+	for(int i = 0; i < num_vertices; i++){
+		g->startList[i] = (Node*)malloc(sizeof(Node));
+	}
+
+	for(int i = 0; i < num_vertices; i++){
+		g->startList[i] = NULL;
+	}
+
+	return g;
+}
+
+Node* createNode(int val){
+	Node* n = (Node*)malloc(sizeof(Node));
+	n->val = val;
+	n->visited = 0;
+	n->next = NULL;
+
+	return n;
+}
+
+// One indexing done (also handles exceptions)
+int addUndirectedEdge(Graph g, int u, int v){
+	nodeptr one = createNode(u);
+	assert(one != NULL);
+	one->next = g->startList[v - 1];
+	g->startList[v - 1] = one;
+
+	nodeptr two = createNode(v);
+	assert(two != NULL);
+	two->next = g->startList[u - 1];
+	g->startList[u - 1] = two;
+
+	return 1;	
+}
+
+int findMinimumUnvisited(Graph g, int from, int initialValue){
+	nodeptr p = g->startList[from - 1];
+	int initialVal = initialValue;
+
+	int minimum = INT_MAX;
+	int test = 0;
+	while(p != NULL){
+		// printf("Hello\n");
+		if(g->startList[p->val - 1]->visited == 0){ // or simple initial p->visited
+			test = p->val;
+		} else{
+			if(p->val == initialVal){
+				p = p->next;
+				continue;
+			}
+			if(p->idkname == initialVal){
+				p = p->next;
+				continue;
+			}
+			p->idkname = initialVal;
+			test = findMinimumUnvisited(g, p->val, initialVal);
+		}
+
+		if(test < minimum){
+			minimum = test;
+		}
+		p = p->next;
+	}
+
+	return minimum;
+}
+
+// The function that does the work that we have to perform
+Array doTheTask(Graph g){
+	Array journal = (Array)malloc(sizeof(int) * g->num_vertices);
+	assert(journal != NULL);
+
+	journal[0] = 1;
+	
+	g->startList[0]->visited = 1;
+
+	/*
+		nodeptr t = findMinimumUnvisited(journal[0]);
+		journal[1] = t->val;
+		nodeptr s = findMinimumUnvisited(journal[1]);
+		journal[2] = s->val;
+
+		// Do this until the journal is full
+	*/
+	int from = 1;
+	int index = 1;
+	for(int i = 1; i < g->num_vertices; i++){
+		int temp = findMinimumUnvisited(g, from, from);
+		// printf("%d\n", temp);
+		g->startList[temp - 1]->visited = 1;
+		journal[index] = temp;
+
+		from = temp;
+		index++;
+	}
+	
+	return journal;
+}
+
+void printGraph(Graph g){
+	for(int i = 0; i < g->num_vertices; i++){
+		nodeptr p = g->startList[i];
+		while(p != NULL){
+			printf("(%d -> %d) ", i + 1, p->val);
+			p = p->next;
+		}
+		printf("\n");
+	}
+}
+
+int main(){
+	int n, m;
+	scanf("%d %d", &n, &m);
+
+	Graph g = initGraph(n);
+
+	int u, v;
+	for(int i = 0; i < m; i++){
+		scanf("%d %d", &u, &v);
+		addUndirectedEdge(g, u, v);
+	}
+
+	// printGraph(g);
+
+	Array journal = doTheTask(g);
+
+	for(int i = 0; i < n; i++){
+		printf("%d ", journal[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
