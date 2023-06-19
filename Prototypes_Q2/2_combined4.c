e50b20be2c9cec3a88b5d4a049529817b6423fe9
anshium <anshchablani@gmail.com>
2023-06-20 03:39:00 +0530

    Added heaps

--- Prototypes_Q2/2_combined4.c
+++ Prototypes_Q2/2_combined4.c
@@ -136,79 +136,6 @@ upDownList initUpDownList(int N, int M){
 
 // as easy as that
 
-void doTheThingdotCom(Graph G, upDownList U, int u, int v){
-	Queue Q = createQueue(2 * 1e6 + 1e3);
-	enqueue(Q, 0);
-
-	int i, j;
-
-	int current, myVal;
-
-	int N = G->N;
-	int M = G->M;
-	U->upDownArray[0 * M + 0] = 0;
-	// Dequeue a cell from the queue.
-	// Check all adjacent cells that can be reached from the current cell (R, C). If an adjacent cell is within the grid boundaries and has a different height, enqueue it.
-	// Update the distance of the adjacent cell if it is not visited or if the new distance is smaller than the previously recorded distance.
-	// Mark the adjacent cell as visited.
-	int l;
-	while(!isEmpty(Q)){
-		l = dequeue(Q);
-		i = l / M;
-		j = l % M;
-
-		// free(l);
-		G->graphArray[l].visited = true;
-		
-		current = G->graphArray[l].value;
-		myVal = U->upDownArray[l];
-
-
-		if(i >= 1){
-			// if(G->graphArray[(i - 1) * M + j].visited == false){
-			// 	enqueue(Q, (i - 1) * M + j);
-			// }
-
-			if(U->upDownArray[(i - 1) * M + j] > myVal){
-				enqueue(Q, (i - 1) * M + j);
-				U->upDownArray[(i - 1) * M + j] = U->upDownArray[i * M + j] + 1 * (current != G->graphArray[(i - 1) * M + j].value);
-			}
-		}
-		if(i < N - 1){
-			// if(G->graphArray[(i + 1) * M + j].visited == false){
-			// 	enqueue(Q, (i + 1) * M + j);
-			// }
-
-			if(U->upDownArray[(i + 1) * M + j] > myVal){
-				enqueue(Q, (i + 1) * M + j);
-				U->upDownArray[(i + 1) * M + j] = U->upDownArray[i * M + j] + 1 * (current != G->graphArray[(i + 1) * M + j].value);
-			}
-		}
-		if(j >= 1){
-			// if(G->graphArray[i * M + (j - 1)].visited == false){
-			// 	enqueue(Q, i * M + (j - 1));
-			// }
-
-			if(U->upDownArray[i * M + (j - 1)] > myVal){
-				enqueue(Q, i * M + (j - 1));
-				U->upDownArray[i * M + (j - 1)] = U->upDownArray[i * M + j] + 1 * (current != G->graphArray[i * M + (j - 1)].value);
-			}
-		}
-		if(j < M - 1){
-			// if(G->graphArray[i * M + (j + 1)].visited == false){
-			// 	enqueue(Q, i * M + (j + 1));
-			// }
-
-			if(U->upDownArray[i * M + (j + 1)] > myVal){
-				enqueue(Q, i * M + (j + 1));
-				U->upDownArray[i * M + (j + 1)] = U->upDownArray[i * M + j] + 1 * (current != G->graphArray[i * M + (j + 1)].value);
-			}
-		}
-	}
-	// free(Q->L);
-	// free(Q->rear);
-	// free(Q);
-}
 
 void print(upDownList U){
 	for(int i = 0; i < U->N; i++){
@@ -231,6 +158,174 @@ void freeUDL(upDownList U){
 	free(U);
 }
 
+struct HeapInfo {
+    int* heap;
+    int capacity;
+    int size;
+};
+
+typedef struct HeapInfo* Heap;
+
+Heap createHeap(int capacity) {
+    Heap heap = (Heap) malloc(sizeof(struct HeapInfo));
+    heap->capacity = capacity;
+    heap->size = 0;
+    heap->heap = (int*)malloc(capacity * sizeof(int));
+    return heap;
+}
+
+int getParent(int i) {
+    int parent = (i - 1) >> 2;
+	return parent;
+}
+
+int getLeftChild(int i) {
+	int leftChild = i << 2 + 1;
+    return leftChild;
+}
+
+int getRightChild(int i) {
+	int rightChild = i << 2 + 2;
+    return rightChild;
+}
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void heapifyUp(Heap heap, int index) {
+    int parentIndex = parent(index);
+    if (index > 0) {
+		if(heap->heap[index] < heap->heap[parentIndex]){
+			swap(&heap->heap[index], &heap->heap[parentIndex]);
+			heapifyUp(heap, parentIndex);
+		}
+    }
+}
+
+void heapifyDown(Heap heap, int index) {
+    int leftChildIndex = leftChild(index);
+    int rightChildIndex = rightChild(index);
+    int smallestIndex = index;
+
+    if (leftChildIndex < heap->size)
+		if(heap->heap[leftChildIndex] < heap->heap[smallestIndex])
+			smallestIndex = leftChildIndex;
+    if (rightChildIndex < heap->size)
+		if(heap->heap[rightChildIndex] < heap->heap[smallestIndex])
+        	smallestIndex = rightChildIndex;
+
+    if (smallestIndex != index) {
+        swap(&heap->heap[index], &heap->heap[smallestIndex]);
+        heapifyDown(heap, smallestIndex);
+    }
+}
+
+void insert(Heap heap, int value) {
+    if (heap->size == heap->capacity) {
+		// heap full
+        return;
+    }
+
+    heap->heap[heap->size] = value;
+    heapifyUp(heap, heap->size);
+    heap->size++;
+
+	return;
+}
+
+int getMin(Heap heap) {
+    if (heap->size == 0) {
+        printf("Heap is empty.\n");
+        return -1;
+    }
+    return heap->heap[0];
+}
+
+void doTheThingdotCom(Graph G, upDownList U, int u, int v){
+	Queue Q = createQueue(2 * 1e6 + 1e3);
+	enqueue(Q, 0);
+
+	int i, j;
+
+	int current, myVal;
+
+	int N = G->N;
+	int M = G->M;
+	U->upDownArray[0 * M + 0] = 0;
+	// Dequeue a cell from the queue.
+	// Check all adjacent cells that can be reached from the current cell (R, C). If an adjacent cell is within the grid boundaries and has a different height, enqueue it.
+	// Update the distance of the adjacent cell if it is not visited or if the new distance is smaller than the previously recorded distance.
+	// Mark the adjacent cell as visited.
+	int l;
+	Heap H;
+	while(!isEmpty(Q)){
+		l = dequeue(Q);
+		i = l / M;
+		j = l % M;
+
+		// free(l);
+		G->graphArray[l].visited = true;
+		
+		current = G->graphArray[l].value;
+		myVal = U->upDownArray[l];
+
+		H = createHeap(G->N * G->M);
+
+		if(i >= 1){
+			// if(G->graphArray[(i - 1) * M + j].visited == false){
+			// 	enqueue(Q, (i - 1) * M + j);
+			// }
+
+			if(U->upDownArray[(i - 1) * M + j] > myVal){
+				// enqueue(Q, (i - 1) * M + j);
+				insert(H, (i - 1) * M + j);
+				U->upDownArray[(i - 1) * M + j] = U->upDownArray[i * M + j] + 1 * (current != G->graphArray[(i - 1) * M + j].value);
+			}
+		}
+		if(i < N - 1){
+			// if(G->graphArray[(i + 1) * M + j].visited == false){
+			// 	enqueue(Q, (i + 1) * M + j);
+			// }
+
+			if(U->upDownArray[(i + 1) * M + j] > myVal){
+				// enqueue(Q, (i + 1) * M + j);
+				insert(H, (i + 1) * M + j);
+				U->upDownArray[(i + 1) * M + j] = U->upDownArray[i * M + j] + 1 * (current != G->graphArray[(i + 1) * M + j].value);
+			}
+		}
+		if(j >= 1){
+			// if(G->graphArray[i * M + (j - 1)].visited == false){
+			// 	enqueue(Q, i * M + (j - 1));
+			// }
+
+			if(U->upDownArray[i * M + (j - 1)] > myVal){
+				// enqueue(Q, i * M + (j - 1));
+				insert(H, i * M + (j - 1));
+				U->upDownArray[i * M + (j - 1)] = U->upDownArray[i * M + j] + 1 * (current != G->graphArray[i * M + (j - 1)].value);
+			}
+		}
+		if(j < M - 1){
+			// if(G->graphArray[i * M + (j + 1)].visited == false){
+			// 	enqueue(Q, i * M + (j + 1));
+			// }
+
+			if(U->upDownArray[i * M + (j + 1)] > myVal){
+				// enqueue(Q, i * M + (j + 1));
+				insert(H, i * M + (j + 1));
+				U->upDownArray[i * M + (j + 1)] = U->upDownArray[i * M + j] + 1 * (current != G->graphArray[i * M + (j + 1)].value);
+			}
+		}
+		enqueue(Q, getMin(H));
+		free(H->heap);
+		free(H);
+	}
+	// free(Q->L);
+	// free(Q->rear);
+	// free(Q);
+}
 int main(){
 	int T;
 	scanf("%d", &T);
