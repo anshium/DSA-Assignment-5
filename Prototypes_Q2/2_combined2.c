b28af7f50834302435d3003e6f6019140b6dbde7
anshium <anshchablani@gmail.com>
2023-06-20 01:15:38 +0530

    Added test cases

--- Prototypes_Q2/2_combined2.c
+++ Prototypes_Q2/2_combined2.c
@@ -303,24 +303,33 @@ void print(upDownList U){
 }
 
 int main(){
+	int T;
+	scanf("%d", &T);
+
 	int N, M;
-	scanf("%d %d", &N, &M);
-	Graph G = initGraph(N, M);
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < M; j++){
-			scanf("%d", &G->graphArray[i][j]);
+	Graph G;
+	upDownList U;
+	AdjacencyMatrix A;
+
+	for(int i = 0; i < T; i++){
+		scanf("%d %d", &N, &M);
+		G = initGraph(N, M);
+		for(int i = 0; i < N; i++){
+			for(int j = 0; j < M; j++){
+				scanf("%d", &G->graphArray[i][j]);
+			}
 		}
+
+		U = initUpDownList(N, M);
+		A = initAdjacencyMatrix(N, M);
+
+		doTheThingdotCom(G, U, A, 0, 0);
+
+		// printf("\n");
+		
+		// print(U);
+
+		printf("%d\n", U->upDownArray[N - 1][M - 1]);
 	}
-
-	upDownList U = initUpDownList(N, M);
-	AdjacencyMatrix A = initAdjacencyMatrix(N, M);
-
-	doTheThingdotCom(G, U, A, 0, 0);
-
-	// printf("\n");
-	
-	// print(U);
-
-	printf("%d\n", U->upDownArray[N - 1][M - 1]);
 	return 0;
 }
